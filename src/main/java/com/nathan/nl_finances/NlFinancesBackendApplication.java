c2ef9c7fccadbdec33c0d64aa76c483183924be2
nathanlls <nathan.llucasl@gmail.com>
2025-04-16 15:29:34 -0300

    init repo

---
+++ src/main/java/com/nathan/nl_finances/NlFinancesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.nl_finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlFinancesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlFinancesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathan/nl_finances/NlFinancesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.nl_finances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlFinancesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
